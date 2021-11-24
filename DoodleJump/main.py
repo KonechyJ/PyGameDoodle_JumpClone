0d7d2d8c225bea9911622f6ade82e4514b140ced
Unknown <Jkonechy@live.com>
2021-11-24 13:43:04 -0800

    Added player Movement
    Using friction and acceleration, added player movement

--- DoodleJump/main.py
+++ DoodleJump/main.py
@@ -4,8 +4,8 @@ from pygame.locals import *
 pygame.init()
 vec = pygame.math.Vector2  # 2 for two dimensional
 
-HEIGHT = 450
-WIDTH = 400
+HEIGHT = 650
+WIDTH = 600
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
@@ -13,7 +13,7 @@ FPS = 60
 FramePerSec = pygame.time.Clock()
 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Doodle Jump Game")
 
 
 class Player(pygame.sprite.Sprite):
@@ -21,7 +21,32 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((128, 255, 40))
-        self.rect = self.surf.get_rect(center=(10, 420))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((10, 385))
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def move(self):
+        self.acc = vec(0, 0)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
 
 
 class platform(pygame.sprite.Sprite):
@@ -45,7 +70,7 @@ while True:
             #sys.exit()
 
     displaysurface.fill((0, 0, 0))
-
+    P1.move()
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
 
